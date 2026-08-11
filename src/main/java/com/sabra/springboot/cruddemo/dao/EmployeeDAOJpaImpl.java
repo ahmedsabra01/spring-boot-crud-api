a3bc29a6ff589cbc15eb250c2ce922e38c8ddba0
ahmedsabra01 <ahmadsabra02@gmail.com>
2026-08-11 05:42:15 +0300

    feat: implement employee CRUD API

---
+++ src/main/java/com/sabra/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,46 @@
+package com.sabra.springboot.cruddemo.dao;
+
+import com.sabra.springboot.cruddemo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOJpaImpl implements EmployeeDAO {
+    private EntityManager entityManager;
+
+
+    @Autowired
+    public EmployeeDAOJpaImpl(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        //create a query -> execute the query -> return the result
+        TypedQuery<Employee> theQuery = entityManager.createQuery("FROM Employee" , Employee.class);
+        List<Employee> employees = theQuery.getResultList();
+        return employees;
+    }
+
+    @Override
+    public Employee findById(int id) {
+        Employee employee = entityManager.find(Employee.class,id);
+        return employee;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        Employee dbEmployee = entityManager.merge(employee);
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Employee employee = entityManager.find(Employee.class,id);
+        entityManager.remove(employee);
+    }
+}
