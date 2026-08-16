26037cae290292ca95fd0cb2e51214b0b8bb03d9
ahmedsabra01 <ahmadsabra02@gmail.com>
2026-08-16 05:00:10 +0300

    feat: add Spring Security with in-memory users

---
+++ src/main/java/com/sabra/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.sabra.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.http.HttpRequest;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails ahmed = User.builder()
+                .username("Ahmed")
+                .password("{noop}ahmed123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails nabil = User.builder()
+                .username("Nabil")
+                .password("{noop}nabil123")
+                .roles("EMPLOYEE","MANAGER")
+                .build();
+
+        UserDetails sabra = User.builder()
+                .username("Sabra")
+                .password("{noop}sabra123")
+                .roles("EMPLOYEE","MANAGER","ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(ahmed,nabil,sabra);
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH,"/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+        //display cross site request forgery csrf not requires in statless rest apis get post put delete or patch
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
